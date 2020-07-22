f9176324e0e6eb385513bb6d9c2d89e3bad2ee76
Danilo de Lucio <47226196+danilodelucio@users.noreply.github.com>
2020-07-22 10:38:30 -0300

    Opção de voltar e Executável

--- sorteador_de_nomes.py
+++ sorteador_de_nomes.py
@@ -1,4 +1,5 @@
 import random
+from time import sleep
 
 
 def linhas():
@@ -21,56 +22,75 @@ while True:
         linhas()
 
         if nome != "":
-            break
+            # INSERINDO MAIS NOMES
+            while True:
+                try:
+                    add_nome = input("Deseja inserir mais um nome? [S/N] ").upper().strip()
 
-        else:
-            msgError()
-            print()
+                    if add_nome == "S":
+                        nome = input("Digite mais um nome: ").title().strip()
+                        lista_nomes.append(nome)
+                        linhas()
+                        continue
 
-    except:
-        msgError()
+                    if add_nome == "N":
+                        # EMBARALHANDO OS NOMES E EXIBINDO NA TELA
+                        print()
+                        print("NOMES REGISTRADOS")
+                        for c in lista_nomes:
+                            print(c)
+                        print()
+                        random.shuffle(lista_nomes)
+                        print(f"Total de {len(lista_nomes)} pessoas registradas.")
+                        print()
 
-while True:
-    try:
-        add_nome = input("Deseja inserir mais um nome? [S/N] ").upper().strip()
+                        # PEGANDO O NÚMERO TOTAL DE PESSOAS PRA DIVIDIR OS TIMES
+                        grupo_1 = dict()
+                        n = 2
+                        n_div = int(len(lista_nomes) / n)
 
-        if add_nome == "S":
-            nome = input("Digite mais um nome: ").title().strip()
-            lista_nomes.append(nome)
-            linhas()
+                        # RESULTADO FINAL DOS TIMES
+                        print("TIME 01")
+                        for c in lista_nomes[:n_div]:
+                            print(c)
+                        print()
+                        print("TIME 02")
+                        for c in lista_nomes[n_div:]:
+                            print(c)
+
+
+                        # OPÇÃO DE CONTINUAR OU NÃO
+                        while True:
+                            try:
+                                print()
+                                linhas()
+                                cont = str(input("Deseja sortear outros nomes? [S/N] ")).upper().strip()
+
+                                if cont == "N" or cont == "S":
+                                    linhas()
+                                    break
+
+                            except:
+                                msgError()
+                        if cont == "N" or cont == "S":
+                            break
+                    else:
+                        msgError()
+                except:
+                    msgError()
+        if cont == "S":
             continue
-
-        if add_nome == "N":
-            # EMBARALHANDO OS NOMES E EXIBINDO NA TELA
-            print()
-            print("NOMES REGISTRADOS")
-            for c in lista_nomes:
-                print(c)
-            print()
-            random.shuffle(lista_nomes)
-            print(f"Total de {len(lista_nomes)} pessoas registradas.")
-            print()
-
-            # PEGANDO O NÚMERO TOTAL DE PESSOAS PRA DIVIDIR OS TIMES
-            grupo_1 = dict()
-            n = 2
-            n_div = int(len(lista_nomes) / n)
-
-            # RESULTADO FINAL DOS TIMES
-            print("TIME 01")
-            for c in lista_nomes[:n_div]:
-                print(c)
-            print()
-            print("TIME 02")
-            for c in lista_nomes[n_div:]:
-                print(c)
+        if cont == "N":
             break
-
         else:
             msgError()
-
+            print()
     except:
         msgError()
+
 
 print()
-print("FIM")
+print("OBRIGADO POR USAR MINHA HUMILDE FERRAMENTA!")
+print("www.danilodelucio.com")
+print("www.github.com/danilodelucio")
+sleep(10)
